20b507c2d18f69764cd8352102eef48c2b8bc38c
Farid Adam <faridibin@gmail.com>
2024-12-02 18:55:54 -0500

    Add PaymentPages service and interface for payment page management

---
+++ src/Contracts/Services/Payments/PaymentPagesInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Faridibin\Paystack\Contracts\Services;
+
+use Faridibin\Paystack\DTOs\Response;
+
+interface PaymentPagesInterface
+{
+    /**
+     * Create a payment page.
+     * Create a payment page on your integration
+     *
+     * @param string $name
+     * @param array $optional
+     * @return \Faridibin\Paystack\DTOs\Response
+     */
+    public function createPaymentPage(string $name, array $optional): Response;
+
+    /**
+     * List Payment Pages.
+     * List payment pages available on your integration.
+     *
+     * @return \Faridibin\Paystack\DTOs\Response
+     */
+    public function listPaymentPages(int $perPage = 50, int $page = 1, array $optional = []): Response;
+
+    /**
+     * Fetch Payment Page
+     * Get details of a payment page on your integration
+     *
+     * @param string $identifier
+     * @return \Faridibin\Paystack\DTOs\Response
+     */
+    public function fetchPaymentPage(string $identifier): Response;
+
+    /**
+     * Update a payment page.
+     * Update a payment page's details on your integration
+     *
+     * @param string $identifier
+     * @param array $data
+     * @return \Faridibin\Paystack\DTOs\Response
+     */
+    public function updatePaymentPage(string $identifier, array $data): Response;
+
+    /**
+     * Check slug availability.
+     * Check if a slug is available for use on your integration
+     *
+     * @param string $slug
+     * @return \Faridibin\Paystack\DTOs\Response
+     */
+    public function checkSlugAvailability(string $slug): Response;
+}
