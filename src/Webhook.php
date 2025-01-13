47413ee878b941d83d4c4f0f65e82021f432e7e0
Farid Adam <faridibin@gmail.com>
2025-01-13 00:20:26 -0500

    Add Webhook class with methods for signature validation and IP whitelisting

---
+++ src/Webhook.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Faridibin\Paystack;
+
+class Webhook
+{
+    // 
+    public static function validateSignature(): self
+    {
+        // 
+        return new self();
+    }
+
+    public static function isIpWhitelisted(): self
+    {
+        // 
+        return new self();
+    }
+}
